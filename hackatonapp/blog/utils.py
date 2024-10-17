73cba39ef0622636c5e0cbd486c40a89f25b2711
jiriceKKK <96952249+jiriceKKK@users.noreply.github.com>
2024-10-17 13:21:09 +0200

    Revert "Hospital - info"
    This reverts commit fa8f9544591a73e05bc744b93dd267cc1cc1aba0.

--- hackatonapp/blog/utils.py
+++ hackatonapp/blog/utils.py
@@ -4,7 +4,6 @@ import json
 
 POLICE_URL = "https://services6.arcgis.com/ogJAiK65nXL1mXAW/arcgis/rest/services/Slu%C5%BEebny_Policie_%C4%8CR/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
 FIRE_URL = "https://services6.arcgis.com/ogJAiK65nXL1mXAW/arcgis/rest/services/Stanice_a_pracoviště_hasičského_záchranného_systému/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
-HOSPITAL_URL = "https://services6.arcgis.com/ogJAiK65nXL1mXAW/arcgis/rest/services/V%C3%BDjezdov%C3%A9_z%C3%A1kladny_zdravotnick%C3%A9_z%C3%A1chrann%C3%A9_slu%C5%BEby/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
 
 def get_police_station_info():
     # Send a get request to the police API
@@ -63,27 +62,7 @@ def get_fire_station_info():
         return [stations_list, data_collected]
     else:
         print("Nepodařilo se načíst informace o požárních stanicích.")
-
-def get_hospital_station_info():
-    # Odeslat GET požadavek na API požární stanice
-    response = requests.get(HOSPITAL_URL)
     
-    # Kontrola, zda byl požadavek úspěšný
-    if response.status_code == 200:
-        # Parsování dat odpovědi jako JSON
-        data = response.json()
-        services_list = []
-
-        try:
-            # Vytvoření seznamu s názvy a souřadnicemi
-            services_list = [(feature['properties']['vyjezdova_zakladna'], feature['geometry']['coordinates']) for feature in data['features']]
-            data_collected = True
-
-        except Exception as e:
-            print(f"Chyba při zpracování dat: {e}")
-            data_collected = False
-
-        return [services_list, data_collected]
 
 
 if __name__ == "__main__":
@@ -91,5 +70,3 @@ if __name__ == "__main__":
     print("Test one passed.")
     assert get_fire_station_info()[1] == True
     print("Test two passed.")
-    assert get_hospital_station_info()[1] == True
-    print("All tests passed.")
